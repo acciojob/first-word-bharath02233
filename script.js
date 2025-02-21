c8e220490578a58c5546ff7ec1fe1db3bcce7d16
bharath02233 <bharath02233@gmail.com>
2025-02-21 16:00:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+   let trimmedStr = str.trim();
+    let spaceIndex = trimmedStr.indexOf(" ");
+
+    // If there is no space, return the entire string
+    return spaceIndex === -1 ? trimmedStr : trimmedStr.slice(0, spaceIndex);
 }
 
 // Do not change the code below
